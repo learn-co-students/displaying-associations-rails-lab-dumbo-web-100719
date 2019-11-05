f4530b434bbfdf7cc89f25de84d9683aa59c414c
Hope <hopegiometti@gmail.com>
2019-11-05 14:36:55 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+#artists
+ariana = Artist.create(name: "Ariana Grande")
+elton = Artist.create(name: "Elton John")
+vamp = Artist.create(name: "Vampire Weekend")
+
+#songs
+break_free = Song.create(title: "Break Free", artist: ariana)
+honky_cat = Song.create(title: "Honky Cat", artist: elton)
+comma = Song.create(title: "Oxford Comma", artist: vamp)
+
